a30e3e1fc94027eae9daac778f869c58a311e354
Renan Souza <falecomrenansouza@gmail.com>
2024-04-05 16:23:24 -0300

    Comentários adicionados ao programa de cálculo de área da circunferência

--- Fundamentos/src/AreaCircunferencia.java
+++ Fundamentos/src/AreaCircunferencia.java
@@ -1,8 +1,9 @@
 public class AreaCircunferencia {
     public static void main(String[] args) {
+        // Declara as variáveis e constantes necessárias para a execução do programa
         double raio = 3.4;
         final double PI = 3.14159;
-
+        // Realiza o cálculo da á area da circunferência e imprime na saída do console
         double area = PI * raio * raio;
         System.out.println(area);
     }
